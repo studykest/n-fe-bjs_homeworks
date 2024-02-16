3f482ffab159072c6716551f90a3a89fa02920c5
studykest <studyke@yandex.ru>
2024-02-16 17:30:10 +0300

    completed 1/2 task in 1.base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d > 0) {
+    arr.push(
+      (-b + Math.sqrt(d)) / (2 * a),
+      (-b - Math.sqrt(d)) / (2 * a)
+    );
+  } else {
+    arr.push(-b / (2 * a));
+  }
+
   return arr;
 }
 
