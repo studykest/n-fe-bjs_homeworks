ea07500e14f695adf568313258e0015e06c8603b
studykest <studyke@yandex.ru>
2024-03-06 17:41:17 +0300

    completed 1/3 task in 2.function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,9 @@
 function getArrayParams(...arr) {
+  let min = Math.min(...arr),
+    max = Math.max(...arr);
+
+  let sum = arr.reduce((num1, num2) => num1 + num2);
+  let avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
@@ -19,6 +24,36 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
 
 }
+
+// -------------------- tests to 1 task --------------------
+
+let textError = '';
+
+let getResultCompare = function (objResult, objExpect) {
+  for (elem in objResult) {
+    if (objResult[elem] !== objExpect[elem]) {
+      textError = `ожидается ${elem}: ${objExpect[elem]}, но получаем ${elem}: ${objResult[elem]}`;
+      return;
+    }
+  }
+  return true;
+}
+
+console.assert(
+  getResultCompare(
+    getArrayParams(-99, 99, 10), { min: -99, max: 99, avg: 3.33 }
+  ), textError
+);
+console.assert(
+  getResultCompare(
+    getArrayParams(1, 2, 3, -100, 10), { min: -100, max: 10, avg: -16.80 }
+  ), textError
+);
+console.assert(
+  getResultCompare(
+    getArrayParams(5), { min: 5, max: 5, avg: 5 }
+  ), textError
+);
