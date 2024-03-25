d535be956d918d04e730583e62fe3c05dc0b2e67
studykest <studyke@yandex.ru>
2024-03-25 10:11:21 +0300

    completed 1/2 tesk in 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) return false;
+  return  arr1.every((elem, index) => elem === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
 }
+
+// -------------------- tests to 1 task --------------------
+
+console.assert(compareArrays([8, 9], [6]) === false);
+console.assert(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]) === false);
+console.assert(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]) === false);
+console.assert(compareArrays([1, 2, 3], [2, 3, 1]) === false);
+console.assert(compareArrays([8, 1, 2], [8, 1, 2]));
