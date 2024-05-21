40009399e8be5fbfe74c6bca504d7710a2a4d122
studykest <studyke@yandex.ru>
2024-05-21 16:33:24 +0300

    completed 1 task in 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks?.length > 0) {
+    let sum = 0;
+    let count = this.marks.length;
+
+    this.marks.forEach(mark => sum += mark);
+
+    return sum / count;
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
 }
