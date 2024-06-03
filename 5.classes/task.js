50906f0e639b0df391e521ec4a5c54d0336bed45
studykest <studyke@yandex.ru>
2024-06-03 13:25:01 +0300

    completed 2/3 task in 5.classes

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,3 +62,49 @@ class DetectiveBook extends Book {
     this.type = "detective";
   }
 }
+
+// -------------------- 2 task --------------------
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    let resSearch = this.books.find((book) => book[type] === value);
+
+    if (resSearch) {
+      return resSearch;
+    }
+    
+    return null;
+  }
+
+  giveBookByName(bookName) {
+    let resSearch = this.books.findIndex((book) => book.name === bookName);
+    
+    if (resSearch >= 0) {
+      return this.books.splice(resSearch, 1)[0];
+    }
+    
+    return null;
+  }
+}
+
+let library = new Library('Библиотека');
+
+library.addBook(new DetectiveBook('Майк Омер', 'Странные игры', 2024, 384));
+library.addBook(new FantasticBook('Анри Софи', 'Король Ардена', 2024, 608));
+library.addBook(new NovelBook('Цвейг Стефан', 'Случай на Женевском озере', 1919, 3));
+
+library.findBookBy('releaseDate', 1919).name;
+let handsBook = library.giveBookByName('Король Ардена');
+handsBook.state -= 50;
+handsBook.fix();
+library.addBook(handsBook);
