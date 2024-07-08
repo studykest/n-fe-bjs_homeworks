2b9616d3d3d4bc2951eb25a711e5f02db07b0772
studykest <studyke@yandex.ru>
2024-07-08 17:22:19 +0300

    completed 1/2 task in 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,18 @@
-﻿
+﻿// -------------------- 1 task --------------------
+function parseCount(val) { 
+  let num = parseFloat(val, 10);
+
+  if (isNaN(val)) {
+    throw new Error('Невалидное значение');
+  }
+
+  return num;
+}
+
+function validateCount(val) {
+  try {
+    return parseCount(val);
+  } catch(err) {
+    return err;
+  }
+}
