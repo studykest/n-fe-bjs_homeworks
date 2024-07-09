e452dc9adbbbf363f4a586f5b78eeea3e1285bec
studykest <studyke@yandex.ru>
2024-07-09 12:03:34 +0300

    completed 2/2 task in 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,41 @@ function validateCount(val) {
     return err;
   }
 }
+
+// -------------------- 2 task --------------------
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if (a + b < c || b + c < a || c + a < b) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const p = this.perimeter / 2;
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Number(s.toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (err) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      } 
+    };
+  }
+}
